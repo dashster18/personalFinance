fea168b653e7e4c3e04c198c2cf5a970f825912e
Nik S <nikesh.srivastava@outlook.com>
2020-11-26 01:01:18 -0500

    Completed EDHEC Course 1 CIR model and Cash vs Zero Coupon bonds lab

--- edhec_risk_kit.py
+++ edhec_risk_kit.py
@@ -5,6 +5,7 @@ from scipy.stats import norm
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 import ipywidgets as widgets
+import math
 
 def drawdown(return_series : pd.Series):
     """Takes a time series of asset returns.
@@ -505,7 +506,7 @@ def discount(t, r):
 def pv(l, r):
     """
     Computes the present value of a sequence of liabilities
-    l is a indexed by the time, and teh values are the amounts of each liability
+    l is a indexed by the time, and the values are the amounts of each liability
     r is the interest rate
     returns the present value of the sequence
     """
@@ -518,3 +519,57 @@ def funding_ratio(assets, liabilities, r):
     Computes the funding ratio fo some assets given liabilities and interest rate
     """
     return assets / pv(liabilities, r)
+
+def inst_to_ann(r):
+    """
+    Converts short rate to an annualized rate
+    """
+    return np.expm1(r)
+
+def ann_to_inst(r):
+    """
+    Converts annualized rate to a short rate
+    """
+    return np.log1p(r)
+
+def cir(n_years=10, n_scenarios=1, a=0.05, b=0.03, sigma=0.05, steps_per_year=12, r_0=None):
+    """
+    Generate random interest raet evolution over time using the CIR model
+    b and r_0 are assumed to be the annualized rates, not the short rate
+    and the returned values are the annualized rates as well
+    """
+    if r_0 is None:
+        r_0 = b
+    r_0 = ann_to_inst(r_0)
+    dt = 1 / steps_per_year
+    num_steps = int(n_years*steps_per_year) + 1 # because n_years might be a float
+    
+    shock = np.random.normal(loc=0, scale=np.sqrt(dt), size=(num_steps, n_scenarios))
+    rates = np.empty_like(shock)
+    rates[0] = r_0
+    
+    # For Price Generation
+    h = math.sqrt(a**2 + 2*sigma**2)
+    prices = np.empty_like(shock)
+    ####
+    
+    def price(ttm, r):
+        _A = ((2*h*math.exp((h+a)*ttm/2))/(2*h+(h+a)*(math.exp(h*ttm)-1)))**(2*a*b/sigma**2)
+        _B = (2*(math.exp(h*ttm)-1))/(2*h + (h+a)*(math.exp(h*ttm)-1))
+        _P = _A*np.exp(-_B*r)
+        return _P
+    prices[0] = price(n_years, r_0)
+    ####
+    
+    for step in range(1, num_steps):
+        r_t = rates[step-1]
+        d_r_t = a*(b - r_t)*dt + sigma*np.sqrt(r_t)*shock[step]
+        rates[step] = abs(r_t + d_r_t)
+        # generate prices at time t as well ...
+        prices[step] = price(n_years-step*dt, rates[step])
+        
+    rates = pd.DataFrame(data=inst_to_ann(rates), index=range(num_steps))
+    ### for prices
+    prices = pd.DataFrame(data=prices, index=range(num_steps))
+    ###
+    return rates, prices
